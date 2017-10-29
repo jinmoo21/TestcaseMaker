f50806dfcc4918e2197de721ff18ed28b31efc0e
Admin <Admin@Dove>
2017-10-29 11:43:40 +0900

    #6 commit

--- TestcaseMaker/src/com/nts/tcm/Controller.java
+++ TestcaseMaker/src/com/nts/tcm/Controller.java
@@ -33,10 +33,15 @@ public class Controller extends JFrame implements ActionListener {
 	private JMenuBar menuBar;
 	private JMenu fileMenu;
 	private final String fileMenuText = "파일";
+	private JMenuItem saveAsBtn;
+	private final String saveAsText = "저장";
+	private JMenuItem exitBtn;
+	private final String exitText = "종료";
+	private JPanel topPanel;
 	private JTextField expressionField;
 	private final String expressionToolTip = "표현식을 입력하거나 버튼을 눌러 표현식을 작성하세요.";
 	private char baseCondition = 'A';
-	private JPanel panel;
+	private JPanel centetPanel;
 	private JButton andBtn;
 	private final String andBtnText = "AND";
 	private final String andBtnToolTip = "AND 연산을 추가합니다.";
@@ -61,10 +66,10 @@ public class Controller extends JFrame implements ActionListener {
 	private JButton mcdcBtn;
 	private final String mcdcBtnText = "MC/DC";
 	private final String mcdcBtnTollTip = "표현식에 대한 MC/DC 을 구합니다.";
+	private JPanel bottomPanel;
 	private DefaultTableModel initialTable;
 	private final String[] columnNames = { "1", "0" };
 	public Object rowData[][];
-	private boolean isMCCCalculated;
 
 	public Controller() {
 		// Frame
@@ -75,82 +80,86 @@ public class Controller extends JFrame implements ActionListener {
 		// 메뉴바
 		menuBar = new JMenuBar();
 		fileMenu = new JMenu(fileMenuText);
-		fileMenu.add(new JMenuItem("저장"));
+		saveAsBtn = new JMenuItem(saveAsText);
+		saveAsBtn.addActionListener(this);
+		fileMenu.add(saveAsBtn);
 		fileMenu.addSeparator();
-		fileMenu.add(new JMenuItem("종료"));
+		exitBtn = new JMenuItem(exitText);
+		exitBtn.addActionListener(this);
+		fileMenu.add(exitBtn);
 		menuBar.add(fileMenu);
 		setJMenuBar(menuBar);
 		// 텍스트 영역
-		JPanel a = new JPanel();
-		a.setLayout(new GridLayout(1, 1));
-		a.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
-		this.add("North", a);
+		topPanel = new JPanel();
+		topPanel.setLayout(new GridLayout(1, 1));
+		topPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
+		this.add("North", topPanel);
 		expressionField = new JTextField();
 		expressionField.setToolTipText(expressionToolTip);
 		expressionField.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 36));
-		a.add(expressionField);
+		topPanel.add(expressionField);
 		// 버튼 영역
-		panel = new JPanel();
-		panel.setLayout(new GridLayout(2, 4, 10, 10));
-		panel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-		this.add("Center", panel);
+		centetPanel = new JPanel();
+		centetPanel.setLayout(new GridLayout(2, 4, 10, 10));
+		centetPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+		this.add("Center", centetPanel);
 		// AND 버튼
 		andBtn = new JButton(andBtnText);
 		andBtn.addActionListener(this);
 		andBtn.setToolTipText(andBtnToolTip);
-		panel.add(andBtn);
+		centetPanel.add(andBtn);
 		// OR 버튼
 		orBtn = new JButton(orBtnText);
 		orBtn.addActionListener(this);
 		orBtn.setToolTipText(orBtnToolTip);
-		panel.add(orBtn);
+		centetPanel.add(orBtn);
 		// ( 버튼
 		leftParenthesisBtn = new JButton(leftParenthesisBtnText);
 		leftParenthesisBtn.addActionListener(this);
 		leftParenthesisBtn.setToolTipText(leftParenthesisBtnToolTip);
-		panel.add(leftParenthesisBtn);
+		centetPanel.add(leftParenthesisBtn);
 		// ) 버튼
 		rightParenthesisBtn = new JButton(rightParenthesisBtnText);
 		rightParenthesisBtn.addActionListener(this);
 		rightParenthesisBtn.setToolTipText(rightParenthesisBtnToolTip);
-		panel.add(rightParenthesisBtn);
+		centetPanel.add(rightParenthesisBtn);
 		// 초기화 버튼
 		clearBtn = new JButton(clearBtnText);
 		clearBtn.addActionListener(this);
 		clearBtn.setToolTipText(clearBtnTollTip);
-		panel.add(clearBtn);
+		centetPanel.add(clearBtn);
 		// 지우기 버튼
 		cancelBtn = new JButton(cancelBtnText);
 		cancelBtn.addActionListener(this);
 		cancelBtn.setToolTipText(cancelBtnTollTip);
-		panel.add(cancelBtn);
+		centetPanel.add(cancelBtn);
 		// MCC 버튼
 		mccBtn = new JButton(mccBtnText);
 		mccBtn.addActionListener(this);
 		mccBtn.setToolTipText(mccBtnTollTip);
-		panel.add(mccBtn);
+		centetPanel.add(mccBtn);
 		// MC/DC 버튼
 		mcdcBtn = new JButton(mcdcBtnText);
 		mcdcBtn.addActionListener(this);
 		mcdcBtn.addActionListener(this);
 		mcdcBtn.setToolTipText(mcdcBtnTollTip);
-		panel.add(mcdcBtn);
+		centetPanel.add(mcdcBtn);
 		// 표
-		JPanel b = new JPanel();
-		b.setLayout(new GridLayout(1, 1));
-		b.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 10));
-		this.add("South", b);
+		bottomPanel = new JPanel();
+		bottomPanel.setLayout(new GridLayout(1, 1));
+		bottomPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 10));
+		this.add("South", bottomPanel);
 		initialTable = new DefaultTableModel(rowData, columnNames);
 		JTable table = new JTable(initialTable);
 		JScrollPane scrollPane = new JScrollPane(table);
-		b.add(scrollPane);
+		bottomPanel.add(scrollPane);
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		StringBuilder sb = new StringBuilder(expressionField.getText());
-		if (!isMCCCalculated && e.getSource().equals(mccBtn)) {
+		if (e.getSource().equals(mccBtn)) {
 			Parser b = new Parser(expressionField.getText());
 			b.parseExpression();
 			MCC a =  new MCC(b.getPostOrder(), b.getOperandSize());
@@ -170,7 +179,6 @@ public class Controller extends JFrame implements ActionListener {
 				}
 				initialTable.addRow(v);
 			}
-			isMCCCalculated = true;
 		} else if (e.getSource().equals(mcdcBtn)) {
 			
 		} else if (e.getSource().equals(andBtn)) {
@@ -195,6 +203,8 @@ public class Controller extends JFrame implements ActionListener {
 			if (sb.length() != 0) {
 				sb.deleteCharAt(sb.length() - 1);
 			}
+		} else if (e.getSource().equals(exitBtn)) {
+			System.exit(0);
 		}
 		expressionField.setText(sb.toString());
 	}
