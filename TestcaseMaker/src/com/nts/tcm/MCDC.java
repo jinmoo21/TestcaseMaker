128a699059d024fb57f276005496797820db6f7b
HanJinmoo <nt11255@nhn.com>
2017-11-12 18:26:16 +0900

    #8 commit

--- TestcaseMaker/src/com/nts/tcm/MCDC.java
+++ TestcaseMaker/src/com/nts/tcm/MCDC.java
@@ -1,26 +1,32 @@
 package com.nts.tcm;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class MCDC {
-	private ArrayList<String> postOrder;
-	private int operandSize;
+	private String expression;
+	private int operandSize = 2;
 	private String[][] oneTable;
 	private String[][] zeroTable;
 	
-	MCDC(ArrayList<String> postOrder, int operandSize) {
-		this.postOrder = postOrder;
-		this.operandSize = operandSize;
+	Set<String[]> oneSet;
+	Set<String[]> zeroSet;
+	
+	MCDC(String expression) {
+		this.expression = expression; 
 		oneTable = new String[this.operandSize][this.operandSize];
 		zeroTable = new String[this.operandSize][this.operandSize];
 	}
 	
-	public void aaaa() {
-		for (int i = 0; i < operandSize; ++i) {
-			oneTable[i][i] = "1";
-			zeroTable[i][i] = "0";
+	private boolean isOperator(String token) {
+		if (token.equals("&") || token.equals("|") || token.equals("(") || token.equals(")")) {
+			return true;
 		}
-		
+		return false;
+	}
+	
+	public void voo() {
 		for (int i = 0; i < operandSize; ++i) {
 			for (int j = 0; j < operandSize; ++j) {
 				System.out.print(oneTable[i][j] + " ");
@@ -34,37 +40,67 @@ public class MCDC {
 			}
 			System.out.println();
 		}
-		
+		System.out.println();
 	}
 	
-	private boolean isOperator(String token) {
-		if (token.equals("&") || token.equals("|") || token.equals("(") || token.equals(")")) {
-			return true;
-		}
-		return false;
-	}
-	
-	public void getResult() {
+	public void foo1() {
 		for (int i = 0; i < operandSize; ++i) {
-			ArrayList<String> temp = new ArrayList<>(postOrder);
-			for (int j = 0, k = 0; j < temp.size() && k < operandSize; ++j) {
-				if (isOperator(temp.get(j))) {
-					continue;
-				}
-				temp.set(j, oneTable[i][k]);
-				k++;
-			}
-			for (int m = 0; temp.size() != 1; ++m) {
-				if (temp.get(m).equals("&")) {
-					// 하나가 null 이면 null 을 1으로
-					if (temp.get(m - 2) == null) {
-						temp.set(m - 2, "1");
-					}
-				} else if (temp.get(m).equals("|")) {
-					// 하나가 null 이면 null 을 0으로
-					
-				}
-			}
+			oneTable[i][i] = "1";
+			zeroTable[i][i] = "0";
 		}
+	}
+	
+	public void foo2() {
+		for (int i = 0; i < operandSize; ++i) {
+			for (int j = i - 1; j >= 0; --j) {
+				int k = j * 2 + 1;
+				if (expression.substring(k, k + 1).equals("&")) {
+					oneTable[i][j] = "1";
+					zeroTable[i][j] = "1";
+				} else if (expression.substring(k, k + 1).equals("|")) {
+					oneTable[i][j] = "0";
+					zeroTable[i][j] = "0";
+				}
+			}
+			for (int j = i + 1; j < operandSize; ++j) {
+				int k = j * 2 - 1;
+				if (expression.substring(k, k + 1).equals("&")) {
+					oneTable[i][j] = "1";
+					zeroTable[i][j] = "1";
+				} else if (expression.substring(k, k + 1).equals("|")) {
+					oneTable[i][j] = "0";
+					zeroTable[i][j] = "0";
+				}
+			}
+				
+		}
+	}
+	
+	public void foo3() {
+		oneSet = new HashSet<String[]>();
+		for (int i = 0; i < operandSize; ++i) {
+			oneSet.add(oneTable[i]);
+		}
+		zeroSet = new HashSet<String[]>();
+		for (int i = 0; i < operandSize; ++i) {
+			zeroSet.add(zeroTable[i]);
+		}
+	}
+	
+	public void voo2() {
+		for (String[] a : oneSet) {
+			for (int i = 0; i < operandSize; ++i) {
+				System.out.print(a[i] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for (String[] a : zeroSet) {
+			for (int i = 0; i < operandSize; ++i) {
+				System.out.print(a[i] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 }
--- TestcaseMaker/src/com/nts/tcm/Main.java
+++ TestcaseMaker/src/com/nts/tcm/Main.java
@@ -3,9 +3,12 @@ package com.nts.tcm;
 public class Main {
 	public static void main(String[] args) {
 		//new Controller();
-		Parser a = new Parser("A|B|C");
-		a.parseExpression();
-		MCDC mcdc = new MCDC(a.getPostOrder(), a.getOperandSize());
-		mcdc.aaaa();
+		MCDC mcdc = new MCDC("A&B");
+		mcdc.foo1();
+		mcdc.voo();
+		mcdc.foo2();
+		mcdc.voo();
+		// mcdc.foo3();
+		// mcdc.voo2();
 	}
 }
