30e1168ea4af763020b05ba54a2d85359c3ea609
HanJinmoo <nt11255@nhn.com>
2017-12-23 16:10:22 +0900

    #16 commit

--- TestcaseMaker/src/com/nts/tcm/Controller.java
+++ TestcaseMaker/src/com/nts/tcm/Controller.java
@@ -14,6 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -25,12 +26,13 @@ import javax.swing.table.DefaultTableModel;
 
 public class Controller extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 4349430279354340298L;
+	private final static String title = "MCC MC/DC 계산기";
 	private final int initialXPosition = 100;
 	private final int initialYPosition = 100;
-	private final int minWidth = 500;
-	private final int minHeight = 650;
-	private final int initialWidth = 500;
-	private final int initialHeight = 650;
+	private final int initialWidth = 330;
+	private final int initialHeight = 190;
+	private final int btnWidth = 75;
+	private final int btnHeight = 35;
 	private JMenuBar menuBar;
 	private JMenu fileMenu;
 	private final String fileMenuText = "파일";
@@ -40,9 +42,11 @@ public class Controller extends JFrame implements ActionListener {
 	private final String exitText = "종료";
 	private JPanel topPanel;
 	private JTextField expressionField;
+	private final int longWidth = 315;
+	private final int expressionFieldHeight = 50; 
 	private final String expressionToolTip = "표현식을 입력하거나 버튼을 눌러 표현식을 작성하세요.";
 	private char baseCondition = 'A';
-	private JPanel centetPanel;
+	private JPanel centerPanel;
 	private JButton andBtn;
 	private final String andBtnText = "AND";
 	private final String andBtnToolTip = "AND 연산을 추가합니다.";
@@ -53,6 +57,8 @@ public class Controller extends JFrame implements ActionListener {
 	private final String wrapInParenthesisBtnText = "( )";
 	private final String wrapInParenthesisBtnToolTip = "선택한 표현식 양옆으로 괄호를 추가합니다.";
 	private JButton showHideBtn;
+	private final int showHideBtnWidth = 45;
+	private final int showHideBtnHeight = 25;
 	private boolean showHideFlag = false;
 	private final String[] showHideBtnText = { "∨", "∧" };
 	private final String[] showHideBtnToolTip = { "표를 펼칩니다.", "표를 접습니다." };
@@ -68,15 +74,15 @@ public class Controller extends JFrame implements ActionListener {
 	private JButton mcdcBtn;
 	private final String mcdcBtnText = "MC/DC";
 	private final String mcdcBtnTollTip = "표현식에 대한 MC/DC 을 구합니다.";
-	private boolean valueFlag = false;
-	private JPanel bottomPanel;
 	private DefaultTableModel initialTable;
-	private final String[] columnNames = { "Result", "TRUE", "FALSE" };
+	private final String[] columnNames = { "TRUE", "FALSE" };
+	private JScrollPane scrollPane;
+	private final int scrollPaneHeight = 150;
 	public Object rowData[][];
 
 	public Controller() {
 		// Frame
-		super("TestCase Maker");
+		super(title);
 		try {
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
@@ -84,7 +90,8 @@ public class Controller extends JFrame implements ActionListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		setMinimumSize(new Dimension(minWidth, minHeight));
+		this.setLayout(null);
+		setResizable(Boolean.FALSE);
 		setBounds(initialXPosition, initialYPosition, initialWidth, initialHeight);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 메뉴바
@@ -103,74 +110,95 @@ public class Controller extends JFrame implements ActionListener {
 		topPanel = new JPanel();
 		topPanel.setLayout(new GridLayout(1, 1));
 		topPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
-		this.add("North", topPanel);
+//		this.add("North", topPanel);
 		// 텍스트필드
 		expressionField = new JTextField();
 		expressionField.setToolTipText(expressionToolTip);
-		expressionField.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 36));
-		topPanel.add(expressionField);
+		expressionField.setFont(new Font("arian", Font.BOLD, 20));
+		expressionField.setBounds(5, 5, longWidth, expressionFieldHeight);
+		this.add(expressionField);
+//		topPanel.add(expressionField);
 		// 버튼 영역
-		centetPanel = new JPanel();
-		centetPanel.setLayout(new GridLayout(2, 4, 10, 10));
-		centetPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-		this.add("Center", centetPanel);
+		centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(2, 4, 10, 10));
+		centerPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+//		this.add("Center", centerPanel);
 		// MCC 버튼
 		mccBtn = new JButton(mccBtnText);
+		mccBtn.setBounds(5, 60, btnWidth, btnHeight);
 		mccBtn.addActionListener(this);
 		mccBtn.setToolTipText(mccBtnTollTip);
-		centetPanel.add(mccBtn);
+		this.add(mccBtn);
+//		centerPanel.add(mccBtn);
 		// MC/DC 버튼
 		mcdcBtn = new JButton(mcdcBtnText);
+		mcdcBtn.setBounds(85, 60, btnWidth, btnHeight);
 		mcdcBtn.addActionListener(this);
 		mcdcBtn.setToolTipText(mcdcBtnTollTip);
-		centetPanel.add(mcdcBtn);
+		this.add(mcdcBtn);
+//		centerPanel.add(mcdcBtn);
 		// 초기화 버튼
 		clearBtn = new JButton(clearBtnText);
+		clearBtn.setBounds(165, 60, btnWidth, btnHeight);
 		clearBtn.addActionListener(this);
 		clearBtn.setToolTipText(clearBtnTollTip);
-		centetPanel.add(clearBtn);
+		this.add(clearBtn);
+//		centerPanel.add(clearBtn);
 		// 지우기 버튼
 		cancelBtn = new JButton(cancelBtnText);
+		cancelBtn.setBounds(245, 60, btnWidth, btnHeight);
 		cancelBtn.addActionListener(this);
 		cancelBtn.setToolTipText(cancelBtnTollTip);
-		centetPanel.add(cancelBtn);
+		this.add(cancelBtn);
+//		centerPanel.add(cancelBtn);
 		// AND 버튼
 		andBtn = new JButton(andBtnText);
+		andBtn.setBounds(5, 100, btnWidth, btnHeight);
 		andBtn.addActionListener(this);
 		andBtn.setToolTipText(andBtnToolTip);
-		centetPanel.add(andBtn);
+		this.add(andBtn);
+//		centerPanel.add(andBtn);
 		// OR 버튼
 		orBtn = new JButton(orBtnText);
+		orBtn.setBounds(85, 100, btnWidth, btnHeight);
 		orBtn.addActionListener(this);
 		orBtn.setToolTipText(orBtnToolTip);
-		centetPanel.add(orBtn);
+		this.add(orBtn);
+//		centerPanel.add(orBtn);
 		// Wrap In Parenthesis 버튼
 		wrapInParenthesisBtn = new JButton(wrapInParenthesisBtnText);
+		wrapInParenthesisBtn.setBounds(165, 100, btnWidth, btnHeight);
 		wrapInParenthesisBtn.addActionListener(this);
 		wrapInParenthesisBtn.setToolTipText(wrapInParenthesisBtnToolTip);
-		centetPanel.add(wrapInParenthesisBtn);
-		// ) 버튼
+		this.add(wrapInParenthesisBtn);
+//		centerPanel.add(wrapInParenthesisBtn);
+		// ShowHide 버튼
 		showHideBtn = new JButton(showHideBtnText[0]);
+		showHideBtn.setBounds(275, 110, showHideBtnWidth, showHideBtnHeight);
 		showHideBtn.addActionListener(this);
 		showHideBtn.setToolTipText(showHideBtnToolTip[0]);
-		centetPanel.add(showHideBtn);
+		this.add(showHideBtn);
+//		centerPanel.add(showHideBtn);
 		// 표 영역
-		bottomPanel = new JPanel();
-		bottomPanel.setLayout(new GridLayout(1, 1));
-		bottomPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 10));
-		this.add("South", bottomPanel);
+//		bottomPanel = new JPanel();
+//		bottomPanel.setBounds(5, 400, expressionFieldWidth, 300);
+//		this.add(bottomPanel);
 		// 표
 //		DefaultTableCellRenderer centerAlignment = new DefaultTableCellRenderer();
 //		centerAlignment.setHorizontalAlignment(JLabel.CENTER);
 		initialTable = new DefaultTableModel(rowData, columnNames);
 		JTable table = new JTable(initialTable);
 //		table.getColumn("Result").setCellRenderer(centerAlignment);
-		table.setPreferredScrollableViewportSize(new Dimension(initialWidth, 100));
-		table.getColumnModel().getColumn(0).setWidth(200);
-		JScrollPane scrollPane = new JScrollPane(table);
-		bottomPanel.add(scrollPane);
-		bottomPanel.setVisible(Boolean.FALSE);
+//		table.setPreferredScrollableViewportSize(new Dimension(expressionFieldWidth, 300));
+//		table.getColumnModel().getColumn(0).setWidth(200);
+		scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(5, 140, longWidth, scrollPaneHeight);
+		scrollPane.setVisible(Boolean.FALSE);
+		this.add(scrollPane);
+//		bottomPanel.add(scrollPane);
+//		bottomPanel.setVisible(Boolean.FALSE);
 		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	@Override
@@ -182,55 +210,65 @@ public class Controller extends JFrame implements ActionListener {
 			initialTable.setNumRows(0);
 			Parser parser = new Parser(expressionField.getText());
 			parser.parseExpression();
-			MCC mcc =  new MCC(parser.getPostOrder(), parser.getOperandSize());
-			mcc.setTable(mcc.getConditions(), mcc.getResult());
-			int size = mcc.getTrueTable().size() >= mcc.getFalseTable().size() ? mcc.getTrueTable().size() : mcc.getFalseTable().size();
-			for (int i = 0; i < size; ++i) {
-				Vector<Item> v = new Vector<Item>();
-				try {
-					v.addElement(mcc.getTrueTable().get(i));
-				} catch (IndexOutOfBoundsException e1) {
-					v.add(null);
+			if (parser.getOperandSize() > 10) {
+				JOptionPane.showMessageDialog(null, "표현식의 조건을 10개 이하로 입력해주세요.", title, JOptionPane.WARNING_MESSAGE);
+			} else {
+				MCC mcc =  new MCC(parser.getPostOrder(), parser.getOperandSize());
+				mcc.setTable(mcc.getConditions(), mcc.getResult());
+				int size = mcc.getTrueTable().size() >= mcc.getFalseTable().size() ? mcc.getTrueTable().size() : mcc.getFalseTable().size();
+				for (int i = 0; i < size; ++i) {
+					Vector<Item> v = new Vector<Item>();
+					try {
+						v.addElement(mcc.getTrueTable().get(i));
+					} catch (IndexOutOfBoundsException e1) {
+						v.add(null);
+					}
+					try {
+						v.addElement(mcc.getFalseTable().get(i));
+					} catch (IndexOutOfBoundsException e1) {
+						v.add(null);
+					}
+					initialTable.addRow(v);
 				}
-				try {
-					v.addElement(mcc.getFalseTable().get(i));
-				} catch (IndexOutOfBoundsException e1) {
-					v.add(null);
-				}
-				initialTable.addRow(v);
+				showHideFlag = Boolean.TRUE;
+				setBounds(getX(),getY(), initialWidth, initialHeight + scrollPaneHeight);
+				scrollPane.setVisible(Boolean.TRUE);
+				showHideBtn.setText(showHideBtnText[1]);
+				showHideBtn.setToolTipText(showHideBtnToolTip[1]);
 			}
-			showHideFlag = Boolean.TRUE;
-			bottomPanel.setVisible(Boolean.TRUE);
-			showHideBtn.setText(showHideBtnText[1]);
-			showHideBtn.setToolTipText(showHideBtnToolTip[1]);
 		} else if (e.getSource().equals(mcdcBtn) && sb.length() != 0) {
 			initialTable.setNumRows(0);
 			Parser parser = new Parser(expressionField.getText());
 			parser.removeParenthesis();
 			parser.parseExpression();
-			MCDC mcdc = new MCDC(parser.getExpression(), parser.getOperandSize());
-			mcdc.setDiagonalValue();
-			mcdc.setNeutralValue();
-			mcdc.setDistinct();
-			int size = mcdc.getTrueTable().size() >= mcdc.getFalseTable().size() ? mcdc.getTrueTable().size() : mcdc.getFalseTable().size();
-			for (int i = 0; i < size; ++i) {
-				Vector<Item> v = new Vector<Item>();
-				try {
-					v.addElement(mcdc.getTrueTable().get(i));
-				} catch (IndexOutOfBoundsException e1) {
-					v.add(null);
+			if (parser.getOperandSize() > 10) {
+				JOptionPane.showMessageDialog(null, "표현식의 조건을 10개 이하로 입력해주세요.", title, JOptionPane.WARNING_MESSAGE);
+			} else {
+				MCDC mcdc = new MCDC(parser.getExpression(), parser.getOperandSize());
+				mcdc.setDiagonalValue();
+				mcdc.setNeutralValue();
+				mcdc.setDistinct();
+				int size = mcdc.getTrueTable().size() >= mcdc.getFalseTable().size() ? mcdc.getTrueTable().size() : mcdc.getFalseTable().size();
+				for (int i = 0; i < size; ++i) {
+					Vector<Item> v = new Vector<Item>();
+					try {
+						v.addElement(mcdc.getTrueTable().get(i));
+					} catch (IndexOutOfBoundsException e1) {
+						v.add(null);
+					}
+					try {
+						v.addElement(mcdc.getFalseTable().get(i));
+					} catch (IndexOutOfBoundsException e1) {
+						v.add(null);
+					}
+					initialTable.addRow(v);
 				}
-				try {
-					v.addElement(mcdc.getFalseTable().get(i));
-				} catch (IndexOutOfBoundsException e1) {
-					v.add(null);
-				}
-				initialTable.addRow(v);
+				showHideFlag = Boolean.TRUE;
+				setBounds(getX(),getY(), initialWidth, initialHeight + scrollPaneHeight);
+				scrollPane.setVisible(Boolean.TRUE);
+				showHideBtn.setText(showHideBtnText[1]);
+				showHideBtn.setToolTipText(showHideBtnToolTip[1]);
 			}
-			showHideFlag = Boolean.TRUE;
-			bottomPanel.setVisible(Boolean.TRUE);
-			showHideBtn.setText(showHideBtnText[1]);
-			showHideBtn.setToolTipText(showHideBtnToolTip[1]);
 		} else if (e.getSource().equals(andBtn)) {
 			if (caretPosition == 0) {
 				sb.insert(caretPosition, baseCondition);
@@ -272,14 +310,18 @@ public class Controller extends JFrame implements ActionListener {
 		} else if (e.getSource().equals(showHideBtn)) {
 			showHideFlag = !showHideFlag;
 			if (showHideFlag) {
-				bottomPanel.setVisible(showHideFlag);
+				setBounds(getX(),getY(), initialWidth, initialHeight + scrollPaneHeight);
+				scrollPane.setVisible(showHideFlag);
 				showHideBtn.setText(showHideBtnText[1]);
 				showHideBtn.setToolTipText(showHideBtnToolTip[1]);
 			} else {
-				bottomPanel.setVisible(showHideFlag);
+				setBounds(getX(),getY(), initialWidth, initialHeight);
+				scrollPane.setVisible(showHideFlag);
 				showHideBtn.setText(showHideBtnText[0]);
 				showHideBtn.setToolTipText(showHideBtnToolTip[0]);
 			}
+		} else if (e.getSource().equals(saveAsBtn)) {
+			
 		}
 		expressionField.setText(sb.toString());
 		expressionField.requestFocus();
